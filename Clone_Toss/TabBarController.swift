41bd9e78fd2532a9fa5786930200d7a5261f4ad2
RoadtoS7 <kshvv975@gmail.com>
2022-07-14 22:53:07 +0900

    TabBar에 border 추가

--- Clone_Toss/TabBarController.swift
+++ Clone_Toss/TabBarController.swift
@@ -16,6 +16,11 @@ class TabBarController: UITabBarController {
 
         self.navigationItem.prompt = "UITabBarController"
         self.setViewControllers(tabViewController, animated: false)
+        self.tabBar.backgroundColor = .white
+        self.tabBar.layer.cornerRadius = 25
+        self.tabBar.layer.borderColor = UIColor.lightGray.cgColor
+        self.tabBar.layer.borderWidth = 1
+        self.tabBar.clipsToBounds = true
     }
 }
 
